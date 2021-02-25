0e42153f6616dc58a03e6f47c99642f123b035ca
DonVitalko <DonVitalko@mail.ru>
2021-02-25 22:06:05 +0300

    Домашняя работа№5

---
+++ src/Employe.java
@@ -0,0 +1,24 @@
+public class Employe {
+
+    String name;
+    String position;
+    String email;
+    String phone;
+    int salary;
+    int age;
+
+
+    Employe(String name, String position, String email, String phone, int salary, int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    void voice(){
+        System.out.println(name + " Работает в должности: " + position + " Электронный ящик: " + email + " Номер телефона: " + phone + " Заработная плата: " + salary + " Возвраст:" + age);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+public static void main(String arg[]){
+        Employe[] personal = new Employe[5];
+        personal[0] = new Employe("Пушкин Александр", "писатель", "apushkin@mail.ru", "89005002134", 30000, 32);
+        personal[1] = new Employe("Лев Толстой", "философ", "TolstiiLev@mail.ru", "89804235434", 60000, 43);
+        personal[2] = new Employe("Геннадий Лещенко", "скульптор", "Lesha@mail.ru", "89523648134", 25000, 58);
+        personal[3] = new Employe("Мустафа Исламович", "политик", "Mustafa@mail.ru", "8984004364", 450000, 28);
+        personal[4] = new Employe("Виктор Преживальский", "солдат", "Pre*aVi@mail.ru", "8854236934", 33000, 37);
+
+        for(int x = 0; x < personal.length; x++){
+                if(personal[x].age > 40){
+                        personal[x].voice();
+                }
+        }
+}
+
+
+
+}
+
